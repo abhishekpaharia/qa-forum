0347f00aae67853459ea3a0018616297bd4824d6
Aviral <aviral.petwal@leadschool.in>
2023-05-05 20:51:41 -0400

    sepolia

--- truffle-config.js
+++ truffle-config.js
@@ -45,7 +45,9 @@
 // const { MNEMONIC, PROJECT_ID } = process.env;
 
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
-
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = "pioneer team satoshi print dial joy tooth wet medal inject buzz muscle";
+const infuraProjectId = "61f46da6f56947d7956c6d0e7198d28d";
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -65,30 +67,39 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    },
-    //
-    // An additional network, but with some advanced options…
-    // advanced: {
-    //   port: 8777,             // Custom port
-    //   network_id: 1342,       // Custom network
-    //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    //   from: <address>,        // Account to send transactions from (default: accounts[0])
-    //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
-    // },
-    //
-    // Useful for deploying to a public network.
-    // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      gas: 4600000
+     },
+     //
+     // An additional network, but with some advanced options…
+     // advanced: {
+     //   port: 8777,             // Custom port
+     //   network_id: 1342,       // Custom network
+     //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+     //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+     //   from: <address>,        // Account to send transactions from (default: accounts[0])
+     //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
+     // },
+     //
+     // Useful for deploying to a public network.
+     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
+     goerli: {
+       provider: () => new HDWalletProvider(mnemonic, `https://sepolia.infura.io/v3/c40ff2a3a1de4454a3c38c7746915204`),
+       network_id: 11155111,
+       gas: 4000000
+       // confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+     },
+     //
+     // Useful for private networks
+     // private: {
+     //   provider: () => new HDWalletProvider(MNEMONIC, `https://network.io`),
+     //   network_id: 2111,   // This network is yours, in the cloud.
+     //   production: true    // Treats this network as if it was a public net. (default: false)
+     // }
     //
     // Useful for private networks
     // private: {
